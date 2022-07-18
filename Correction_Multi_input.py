acdb8a9b3f72710cdd6d574f9a554b418bf2b893
Yonsei-MILab <52816867+Yonsei-MILab@users.noreply.github.com>
2022-07-18 14:08:42 +0900

    Update Correction_Multi_input.py

--- Correction_Multi_input.py
+++ Correction_Multi_input.py
@@ -5,20 +5,92 @@ from tensorflow.keras import layers
 import tensorflow.keras.backend as K
 import numpy as np
 
-from Correction_UNet import cbam_block   
-
-
 IMAGE_ORDERING_CHANNELS_LAST = "channels_last"
 IMAGE_ORDERING_CHANNELS_FIRST = "channels_first"
 
 # Default IMAGE_ORDERING = channels_last
 IMAGE_ORDERING = IMAGE_ORDERING_CHANNELS_LAST
 
-
 if IMAGE_ORDERING == 'channels_first':
 	MERGE_AXIS = 1
 elif IMAGE_ORDERING == 'channels_last':
 	MERGE_AXIS = -1
+	
+# CBAM --------------------------------------------	
+# Convolutional Block Attention Module(CBAM) block    
+def cbam_block(cbam_feature, ratio=8):	
+	cbam_feature = channel_attention(cbam_feature, ratio)
+	cbam_feature = spatial_attention(cbam_feature)
+	return cbam_feature
+
+def channel_attention(input_feature, ratio=8):
+	
+	channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+	channel = input_feature.shape[channel_axis]  # input_feature._keras_shape[channel_axis]
+	
+	shared_layer_one = Dense(channel//ratio,
+							 activation='relu',
+							 kernel_initializer='he_normal',
+							 use_bias=True,
+							 bias_initializer='zeros')
+	shared_layer_two = Dense(channel,
+							 kernel_initializer='he_normal',
+							 use_bias=True,
+							 bias_initializer='zeros')
+	
+	avg_pool = GlobalAveragePooling2D()(input_feature)    
+	avg_pool = Reshape((1,1,channel))(avg_pool)
+	assert avg_pool.shape[1:] == (1,1,channel)
+	avg_pool = shared_layer_one(avg_pool)
+	assert avg_pool.shape[1:] == (1,1,channel//ratio)
+	avg_pool = shared_layer_two(avg_pool)
+	assert avg_pool.shape[1:] == (1,1,channel)
+	
+	max_pool = GlobalMaxPooling2D()(input_feature)
+	max_pool = Reshape((1,1,channel))(max_pool)
+	assert max_pool.shape[1:] == (1,1,channel)
+	max_pool = shared_layer_one(max_pool)
+	assert max_pool.shape[1:] == (1,1,channel//ratio)
+	max_pool = shared_layer_two(max_pool)
+	assert max_pool.shape[1:] == (1,1,channel)
+	
+	cbam_feature = Add()([avg_pool,max_pool])
+	cbam_feature = Activation('sigmoid')(cbam_feature)
+	
+	if K.image_data_format() == "channels_first":
+		cbam_feature = Permute((3, 1, 2))(cbam_feature)
+	
+	return multiply([input_feature, cbam_feature])
+
+def spatial_attention(input_feature):
+	kernel_size = 7
+	
+	if K.image_data_format() == "channels_first":
+		channel = input_feature._keras_shape[1]
+		cbam_feature = Permute((2,3,1))(input_feature)
+	else:
+		channel = input_feature.shape[-1]
+		cbam_feature = input_feature
+	
+	avg_pool = Lambda(lambda x: K.mean(x, axis=3, keepdims=True))(cbam_feature)
+	assert avg_pool.shape[-1] == 1
+	max_pool = Lambda(lambda x: K.max(x, axis=3, keepdims=True))(cbam_feature)
+	assert max_pool.shape[-1] == 1
+	concat = Concatenate(axis=3)([avg_pool, max_pool])
+	assert concat.shape[-1] == 2
+	cbam_feature = Conv2D(filters = 1,
+					kernel_size=kernel_size,
+					strides=1,
+					padding='same',
+					activation='sigmoid',
+					kernel_initializer='he_normal',
+					use_bias=False)(concat)	
+	assert cbam_feature.shape[-1] == 1
+	
+	if K.image_data_format() == "channels_first":
+		cbam_feature = Permute((3, 1, 2))(cbam_feature)
+		
+	return multiply([input_feature, cbam_feature])	
     
 def UNet(img_input):
 	k1 = 32
